205b2b4dcb35f55e084234fd162e2e461eda5eb8
CBeauford5 <103590886+CBeauford5@users.noreply.github.com>
2023-06-14 11:31:25 -0400

    Update models.js
    changed 'name' to 'username' in schema

--- models.js
+++ models.js
@@ -19,7 +19,7 @@ let movieSchema = mongoose.Schema({
 });
 
 let userSchema = mongoose.Schema({
-  name: {type: String, required: true},
+  username: {type: String, required: true},
   password: {type: String, required: true},
   email: {type: String, required: true},
   birthDate: Date,
